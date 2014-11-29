bac64ad472248ccfe153787ea87458f9d6e479a5
ZanyK <zaneta.kucerova@gmail.com>
2014-11-29 18:21:41 +0100

    Prvni commit

---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,6 @@
+module TasksHelper
+
+	def my_time(blbost)
+		blbost.strftime("%d-%m-%Y-%Hh")
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todolist::Application.config.secret_key_base = '3f1a72553ae797ce74fed5dabd6e7ffa6bed9b3e7122bfddf517c6b0e5fab54b76ce08ca1f1904ba7fe078597110f2194bcb62cea4ab1266049144f610e8bd06'
